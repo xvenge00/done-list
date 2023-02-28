2c644f5808a861e1ad02f620ee9e148144fc1b87
xvenge00 <jawe1222@gmail.com>
2023-02-28 22:17:24 +0100

    date

---
+++ src/routes/[[date]]/+page.server.ts
@@ -0,0 +1,78 @@
+import { type ServerLoadEvent, type Actions, error } from "@sveltejs/kit";
+import { deleteItemRequest, doneItems, newDoneItemRequest, type DoneItems } from "$lib/../routes/api/done/api"
+import * as logger from "$lib/logger"
+import { prisma } from "$lib/prisma";
+import { toISOString } from "$lib/date";
+import { date } from 'yup';
+
+
+function getDateFromParam(dateParam: string | undefined): string {
+    let date = dateParam;
+    if (date === undefined) {
+        date = toISOString(new Date())
+    }
+
+    return date;
+}
+
+async function validateParam(dateParam: string) {
+    try {
+        await date().required().validate(dateParam);
+    } catch(e: any) {
+        throw error(404, "invalid date")
+    }
+}
+
+export async function load({ params }: ServerLoadEvent)  {
+    // logger.info("params.date", params.date);
+    let date = getDateFromParam(params.date);
+    await validateParam(date);
+    
+    logger.info("date", date);
+    let done_items = await prisma.done.findMany({
+        where: {
+            date: date
+        }
+    });
+
+    return {
+        done_items: done_items,
+        date: date
+    }
+}
+
+export const actions: Actions = {
+	create: async ({ request, params }) => {
+        logger.info("params.date", params.date);
+        let date = getDateFromParam(params.date);
+        await validateParam(date);
+
+		const data = Object.fromEntries(await request.formData());
+        logger.info("creating done item: ", JSON.stringify(data));
+        let new_done_item = await newDoneItemRequest.validate(data)
+		
+        await prisma.done.create({
+			data: {
+				created_at: new Date(),
+                date: date,
+				text: new_done_item.text
+			}
+		});
+        logger.info("created new item");
+	},
+    delete: async ({ request }) => {
+        const data = Object.fromEntries(await request.formData());
+        logger.info("deleting done item: ", JSON.stringify(data));
+        let itemToDelete = await deleteItemRequest.validate(data);
+
+        await prisma.done.delete({
+			where: {
+                uid: itemToDelete.uid
+            }
+		});
+    }
+};
+
+// export function match(value) {
+// 	return /^[0-9a-f]{6}$/.test(value);
+// }
