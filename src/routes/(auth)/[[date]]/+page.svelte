205ae1bfe0e912c439a7b3444803474d99163626
xvenge00 <jawe1222@gmail.com>
2023-03-05 23:26:34 +0100

    style

--- src/routes/(auth)/[[date]]/+page.svelte
+++ src/routes/(auth)/[[date]]/+page.svelte
@@ -28,47 +28,105 @@
 </script>
 
 <style>
+    input.text-input {
+        border: 10pt;
+        background-color: black;
+        color: white;
+        border-radius: 15px;
+        padding: 10px;
+        margin: 10px;
+        height: 64px;
+        width: 100%;
+    }
+
+    input.done-button {
+        background-color: rgb(198, 67, 67);
+        border-color: black;
+        color: white;
+        margin: 10px;
+        height: 64px;
+        width: 64px;
+        border-radius: 32px;
+    }
+
+    form.in {
+        display: flex;
+    }
+
+    div.date {
+        display: flex;
+        margin: auto;
+        width: 30%;
+    }
+
+    .date-picker {
+        margin: auto;
+    }
+
+    .center {
+        margin: auto;
+        width: 100%;
+    }
+
+    div.lists {
+        width: 100%;
+    }
+
     ul.list {
-        width: 80%;
-        height: 40px;
-        padding: 5px; 
-        position: absolute;
+        width: 100%;
     }
 
     li.list-item {
-        width: 80%;
-        height: 40px;
-        margin: 5px;
-        border: 1px solid black; 
+        height: 80px;
+        margin: 20px 10px 20px 10px;
+        padding: 20px;
+        border-radius: 15px;
+        background: linear-gradient(180deg, rgba(233,233,233,1) 0%, rgba(255,255,255,1) 100%);
+        box-shadow: 2px 2px 8px;
+    }
+    
+    form.line {
+        display: flex;
+    }
+
+    span.text {
+        margin: auto auto auto 0;
     }
 </style>
 
 
 <title>Done list</title>
 
-<h1>Done list</h1>
+<div class="center">
+    <div class="date">
+        <button on:click={dayBefore}>&lt</button>
+        <input class="date-picker" type="date" bind:value={data.date} on:change={dateChanged}>
+        <button on:click={dayAfter}>&gt</button>
+    </div>
 
-<div>
-    <button on:click={dayBefore}>&lt</button>
-    <input type="date" bind:value={data.date} on:change={dateChanged}>
-    <button on:click={dayAfter}>&gt</button>
-
-    <form method="POST" action="?/create">
-        <input name="text" />
-        <input type="submit" value="I've done this"/>
+    <form method="POST" class="in" action="?/create">
+        <!-- <input name="text" placeholder="Got out of bed?" class="input input-lg input-bordered" autocomplete="off" id="main-input" /> -->
+        <input name="text" placeholder="Got out of bed?" class="text-input" autocomplete="off" />
+        <!-- <input class="btn btn-lg btn-active btn-primary" type="submit" value="I've done this"/> -->
+        <input class="done-button" type="submit" value="Done!"/>
     </form>
 
-    <ul class="list">
-        {#each data.done_items as item}
-        <li class="list-item">
-            <div class="list">
-                <form method="POST" action="?/delete">
-                    {item.text}
-                    <input type="hidden" name="uid" value={item.uid} />
-                    <button aria-label="Mark as complete">x</button>
-                </form>
-            </div>
-        </li>
-        {/each}
-    </ul>
+    <div class="lists">
+        <ul class="list">
+            {#each data.done_items as item}
+            <li class="list-item">
+                <div>
+                    <form class="line" method="POST" action="?/delete">
+                        <span class="text">{item.text}</span>
+                        <input type="hidden" name="uid" value={item.id} />
+                        <!-- <button aria-label="Mark as complete">x</button> -->
+                        <button class="btn btn-circle btn-outline">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" color="red" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                        </button>
+                    </form>
+                </div>
+            </li>
+            {/each}
+        </ul>
+    </div>
 </div>
