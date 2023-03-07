d0a4dbf8cefbe3606b6099e4da8e00634e8c559a
xvenge00 <jawe1222@gmail.com>
2023-03-07 22:26:25 +0100

    add icons

--- src/routes/(public)/welcome/+page.svelte
+++ src/routes/(public)/welcome/+page.svelte
@@ -4,7 +4,7 @@
 <div class="grid h-screen place-content-center">
     <div class="grid place-items-center">
         <h1 class="text-8xl">Done List</h1>
-        <p class="text-xl p-4">You can track what you've done.</p>
+        <p class="text-xl p-4">Write down your achievements, to maximalize your dose of dopamine.</p>
         <a class="btn w-64 rounded-full flex flex-row min-v-screen justify-center items-center" href="/auth/signin">Sign In</a>
     </div>
 </div>
