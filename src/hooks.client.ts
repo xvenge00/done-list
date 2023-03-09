c19b4e8ba054ffb341383c98b792794ae54b4b10
xvenge00 <jawe1222@gmail.com>
2023-03-09 20:05:46 +0100

    format

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,22 +1,22 @@
-import * as SentrySvelte from "@sentry/svelte"
-import type { HandleClientError } from "@sveltejs/kit"
-import {PUBLIC_SENTRY_DSN} from "$env/static/public"
+import * as SentrySvelte from '@sentry/svelte';
+import type { HandleClientError } from '@sveltejs/kit';
+import { PUBLIC_SENTRY_DSN } from '$env/static/public';
 
 SentrySvelte.init({
-    dsn: PUBLIC_SENTRY_DSN,
+	dsn: PUBLIC_SENTRY_DSN,
 
-    // Set tracesSampleRate to 1.0 to capture 100%
-    tracesSampleRate: 1.0,
-})
+	// Set tracesSampleRate to 1.0 to capture 100%
+	tracesSampleRate: 1.0
+});
 
 export const handleError: HandleClientError = ({ error, event }) => {
-    const errorId = crypto.randomUUID()
-    SentrySvelte.captureException(error, {
-        contexts: { sveltekit: { event, errorId } }
-    })
+	const errorId = crypto.randomUUID();
+	SentrySvelte.captureException(error, {
+		contexts: { sveltekit: { event, errorId } }
+	});
 
-    return {
-        message: "Unexpected error",
-        errorId,
-    }
-}
+	return {
+		message: 'Unexpected error',
+		errorId
+	};
+};
--- src/routes/(public)/welcome/+page.server.ts
+++ src/routes/(public)/welcome/+page.server.ts
@@ -10,5 +10,5 @@ export const load: PageServerLoad = async ({ locals }) => {
 
 	return {
 		session
-	}
+	};
 };
