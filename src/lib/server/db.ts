83f471107b02ae8699e8e74c1d183448e0fb98f3
xvenge00 <jawe1222@gmail.com>
2023-03-14 00:09:33 +0100

    more efficient db

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -17,34 +17,36 @@ export async function getDoneItemsForDate(email: string, date: string): Promise<
 }
 
 export async function createPostAt(email: string, text: string, date: string) {
-	let user = await prisma.user.findUnique({
-		where: {
-			email: email
-		}
-	});
-
-	if (!user) {
-		user = await prisma.user.create({
-			data: {
-				email: email
-			}
-		});
-	}
-
 	await prisma.done.create({
 		data: {
 			created_at: new Date(),
 			date: date,
 			text: text,
-			userId: user.id
+			user: {
+				connectOrCreate: {
+					where: {
+						email: email
+					},
+					create: {
+						email: email
+					}
+				}
+			}
 		}
 	});
 }
 
 export async function deletePost(email: string, itemUid: number) {
 	logger.info('deleting done item: ', JSON.stringify(itemUid));
+	await prisma.done.findFirstOrThrow({
+		where: {
+			id: itemUid,
+			user: {
+				email: email
+			}
+		}
+	});
 
-	// TODO make sure the post belongs to email
 	await prisma.done.delete({
 		where: {
 			id: itemUid
